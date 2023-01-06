c95a9d5d8102160100095f7ef50e0666223a5126
Enyel019 <[el_dominic01@hotmail.com]>
2023-01-06 10:21:00 -0400

    update

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -14,15 +14,20 @@ void bubble_sort(int *array, size_t size)
 
 	size_t i = 0, j = 0;
 
+	/* Checking if the size of the array is less than 2. */
 	if (size < 2)
 		return;
+	/* This is the first loop that will iterate through the array. */
 	for (i = 0; i < size - 1; i++)
 	{
 
+		/* This is the second loop that will iterate through the array. */
 		for (j = 0; j < size - i - 1; j++)
 		{
+			/* This is comparing the value of the current element with the next element. */
 			if (array[j] > array[j + 1])
 			{
+				/* Switching the values of the array and printing the array. */
 				switch_a(&array[j], &array[j + 1]);
 				print_array(array, size);
 			}
@@ -37,6 +42,7 @@ void bubble_sort(int *array, size_t size)
  *
  */
 
+/* Switching the values of the array. */
 void switch_a(int *p_a, int *p_b)
 {
 
--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -6,23 +6,29 @@
  * @size: Size of the array
  *
  */
+
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min;
 	bool sort = false;
 
+	/* Iterating through the array. */
 	for (i = 0; i < size - 1; i++)
 	{
 		min = i;
 		sort = false;
+		/* Iterating through the array to find the minimum value. */
 		for (j = i + 1; j < size; j++)
 		{
+			/* Finding the minimum value in the array. */
 			if (array[j] < array[min])
 			{
 				min = j;
 				sort = true;
 			}
 		}
+		/* This is checking if the array is sorted. If it is not sorted, 
+		it will switch the values and print the array. */
 		if (sort)
 		{
 		switch_a(&array[min], &array[i]);
@@ -37,6 +43,7 @@ void selection_sort(int *array, size_t size)
  * @p_b: pointer to second position
  *
  */
+/* Switching the values of the array. */
 void switch_a(int *p_a, int *p_b)
 {
 	int temp;
